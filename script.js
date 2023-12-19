9006e7b547ca276d117f09779565239aafb50fe8
Alex12345H <152187598+Alex12345H@users.noreply.github.com>
2023-12-19 16:04:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,79 @@
+const gameContainer = document.querySelector('.game-container');
 const player = document.getElementById('player');
 const goal = document.getElementById('goal');
-const gameContainer = document.querySelector('.game-container');
 const scoreCounter = document.getElementById('scoreCounter');
 const levelCounter = document.getElementById('levelCounter');
+const leaderboardList = document.getElementById('leaderboardList');
+const nameInput = document.getElementById('nameInput');
 
 let level = 1;
-let obstaclesCount = 1;
 let score = 0;
+let leaderboardData = [];
 
-gameContainer.addEventListener('mousemove', function(event) {
-  const x = event.clientX - gameContainer.getBoundingClientRect().left - player.offsetWidth / 2;
-  const y = event.clientY - gameContainer.getBoundingClientRect().top - player.offsetHeight / 2;
-
-  const maxX = gameContainer.offsetWidth - player.offsetWidth;
-  const maxY = gameContainer.offsetHeight - player.offsetHeight;
-
-  const boundedX = Math.min(Math.max(0, x), maxX);
-  const boundedY = Math.min(Math.max(0, y), maxY);
-
-  player.style.left = boundedX + 'px';
-  player.style.top = boundedY + 'px';
-
-  const playerRect = player.getBoundingClientRect();
-  const goalRect = goal.getBoundingClientRect();
-  const obstacles = document.querySelectorAll('.obstacle');
-
-  if (isColliding(playerRect, goalRect)) {
-    level++;
-    obstaclesCount++;
-    score += 30;
-    alert('Level ' + level);
-    setLevel(level, obstaclesCount);
-    updateScore();
-    updateLevel();
-  }
-
-  for (let obstacle of obstacles) {
-    const obstacleRect = obstacle.getBoundingClientRect();
-    if (isColliding(playerRect, obstacleRect)) {
-      obstacleCollision();
-      alert('Du hast das Hindernis berührt! Versuche es erneut.');
-      setLevel(level, obstaclesCount);
-      break;
-    }
-  }
-});
-
-function isColliding(a, b) {
-  return !(
-    a.bottom < b.top ||
-    a.top > b.bottom ||
-    a.right < b.left ||
-    a.left > b.right
-  );
-}
-
-function setLevel(level, obstaclesCount) {
-  const goalPosition = getNewPosition();
-  goal.style.top = goalPosition.top + 'px';
-  goal.style.left = goalPosition.left + 'px';
-
-  const gameObstacles = document.querySelectorAll('.obstacle');
-  for (let obstacle of gameObstacles) {
-    obstacle.remove();
-  }
-
-  for (let i = 0; i < obstaclesCount; i++) {
-    createObstacle();
-  }
-}
-
-function getNewPosition() {
+function getRandomPosition() {
   const posX = Math.floor(Math.random() * (gameContainer.offsetWidth - 50));
   const posY = Math.floor(Math.random() * (gameContainer.offsetHeight - 50));
-
-  return { top: posY, left: posX };
-}
-
-function createObstacle() {
-  const obstacle = document.createElement('div');
-  obstacle.className = 'obstacle';
-  obstacle.style.top = Math.random() * (gameContainer.offsetHeight - 40) + 'px';
-  obstacle.style.left = Math.random() * (gameContainer.offsetWidth - 40) + 'px';
-  gameContainer.appendChild(obstacle);
-}
-
-function obstacleCollision() {
-  score -= 10;
-  updateScore();
+  return { x: posX, y: posY };
 }
 
 function updateScore() {
-  scoreCounter.textContent = 'Score: ' + score;
+  scoreCounter.textContent = `Score: ${score}`;
 }
 
 function updateLevel() {
-  levelCounter.textContent = 'Level: ' + level;
+  levelCounter.textContent = `Level: ${level}`;
 }
 
-setLevel(level, obstaclesCount);
+function updateLeaderboard() {
+  leaderboardList.innerHTML = '';
+  leaderboardData.sort((a, b) => b.score - a.score);
+
+  leaderboardData.slice(0, 10).forEach((entry, index) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${index + 1}. ${entry.name}: ${entry.score}`;
+    leaderboardList.appendChild(listItem);
+  });
+}
+
+function addToLeaderboard() {
+  const playerName = nameInput.value.trim();
+  if (playerName !== '') {
+    leaderboardData.push({ name: playerName, score });
+    nameInput.value = '';
+    updateLeaderboard();
+  }
+}
+
+function movePlayer(event) {
+  const maxX = gameContainer.offsetWidth - player.offsetWidth;
+  const maxY = gameContainer.offsetHeight - player.offsetHeight;
+
+  const x = Math.min(Math.max(0, event.clientX - gameContainer.getBoundingClientRect().left), maxX);
+  const y = Math.min(Math.max(0, event.clientY - gameContainer.getBoundingClientRect().top), maxY);
+
+  player.style.left = x + 'px';
+  player.style.top = y + 'px';
+
+  const playerRect = player.getBoundingClientRect();
+  const goalRect = goal.getBoundingClientRect();
+
+  if (isColliding(playerRect, goalRect)) {
+    score += 10;
+    level++;
+    updateScore();
+    updateLevel();
+    const newPos = getRandomPosition();
+    goal.style.left = newPos.x + 'px';
+    goal.style.top = newPos.y + 'px';
+  }
+}
+
+function isColliding(a, b) {
+  return !(a.bottom < b.top || a.top > b.bottom || a.right < b.left || a.left > b.right);
+}
+
+gameContainer.addEventListener('mousemove', movePlayer);
+
+updateScore();
+updateLevel();
+updateLeaderboard();
