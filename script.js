e377758c53c4c3b054cccae21458b2f9c6d8f214
Alex12345H <152187598+Alex12345H@users.noreply.github.com>
2023-12-19 21:13:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,57 +14,81 @@ function updateLevel() {
   levelCounter.textContent = `Level: ${level}`;
 }
 
-function createObstacle() {
-  const obstacle = document.createElement('div');
-  obstacle.className = 'obstacle';
-  gameContainer.appendChild(obstacle);
-  return obstacle;
+// Funktion zur Überprüfung von Kollisionen
+function isColliding(element1, element2) {
+  const rect1 = element1.getBoundingClientRect();
+  const rect2 = element2.getBoundingClientRect();
+  return !(
+    rect1.bottom < rect2.top ||
+    rect1.top > rect2.bottom ||
+    rect1.right < rect2.left ||
+    rect1.left > rect2.right
+  );
 }
 
+// Funktion zur Platzierung des Ziels
 function setGoalPosition() {
-  let goalX, goalY;
+  let randomX, randomY;
   do {
-    goalX = Math.random() * (gameContainer.offsetWidth - 30);
-    goalY = Math.random() * (gameContainer.offsetHeight - 30);
-    goal.style.left = goalX + 'px';
-    goal.style.top = goalY + 'px';
-  } while (checkCollision(goal, player));
+    randomX = Math.floor(Math.random() * (gameContainer.offsetWidth - 30));
+    randomY = Math.floor(Math.random() * (gameContainer.offsetHeight - 30));
+  } while (isOverlapping(randomX, randomY));
 
+  goal.style.left = randomX + 'px';
+  goal.style.top = randomY + 'px';
+}
+
+// Funktion zur Überprüfung von Überlappungen mit Hindernissen
+function isOverlapping(x, y) {
+  // Implementierung der Überlappungsprüfung
+  return false; // Platzhalter - Ihre Kollisionslogik hier einfügen
+}
+
+// Funktion zur Generierung von Hindernissen
+function generateObstacles() {
+  // Hindernisse generieren und positionieren
+  // ...
+
+  // Beispiel:
+  const obstacle = document.createElement('div');
+  obstacle.className = 'obstacle';
+  obstacle.style.left = '200px'; // Beispielposition - ändern Sie dies entsprechend Ihrer Logik
+  obstacle.style.top = '100px'; // Beispielposition - ändern Sie dies entsprechend Ihrer Logik
+  gameContainer.appendChild(obstacle);
+}
+
+// Spiel-Logik
+function gameLogic() {
+  // Spielerbewegung, Kollisionserkennung, etc.
+  // ...
+
+  if (isColliding(player, goal)) {
+    // Spieler hat das Ziel erreicht
+    score += 30;
+    level++;
+    updateScore();
+    updateLevel();
+    setGoalPosition();
+    generateObstacles();
+    alert('Hurra! Du hast das Level geschafft!');
+  }
+
+  // Kollisionserkennung mit Hindernissen
   const obstacles = document.querySelectorAll('.obstacle');
-  obstacles.forEach(obstacle => {
-    if (checkCollision(goal, obstacle)) {
-      setGoalPosition(); // Falls das Ziel in einem Hindernis generiert wird, erneut positionieren
+  obstacles.forEach((obstacle) => {
+    if (isColliding(player, obstacle)) {
+      // Kollision mit Hindernis
+      score -= 10;
+      updateScore();
+      alert('Oh nein! Du hast ein Hindernis berührt!');
+      // Weitere Aktionen für eine Kollision mit einem Hindernis hier ausführen
     }
   });
 }
 
-function checkCollision(a, b) {
-  const aRect = a.getBoundingClientRect();
-  const bRect = b.getBoundingClientRect();
-  return !(
-    aRect.bottom < bRect.top ||
-    aRect.top > bRect.bottom ||
-    aRect.right < bRect.left ||
-    aRect.left > bRect.right
-  );
-}
-
-function generateObstacles() {
-  for (let i = 0; i < 2 * level; i++) {
-    const obstacle = createObstacle();
-    let obstacleX, obstacleY;
-    do {
-      obstacleX = Math.random() * (gameContainer.offsetWidth - 30);
-      obstacleY = Math.random() * (gameContainer.offsetHeight - 30);
-      obstacle.style.left = obstacleX + 'px';
-      obstacle.style.top = obstacleY + 'px';
-    } while (checkCollision(obstacle, player) || checkCollision(obstacle, goal));
-  }
-}
-
-// Weitere Spiel-Logik hier implementieren...
-
+// Spiel-Loop starten
 setGoalPosition();
 generateObstacles();
 updateScore();
 updateLevel();
+gameLogic();
